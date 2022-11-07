d8755adbc9f8b30603d5eff8f30965863a32e27c
mangga_asem <erteka@ertekas-iMac.local>
2022-11-07 13:40:32 +0700

    1st push

---
+++ admin/src/components/boxPortf/BoxPortf.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./boxPortf.css";
+
+const BoxPortf = ({ images, title, handleUpdate, handleDelete }) => {
+  return (
+    <div className="boxContainer">
+      <div className="boxWrapper">
+        <div className="boxTitle">{title}</div>
+        <img src={images} alt="" className="boxImage" />
+        <div className="btnWrapper">
+          <button className="btnBox" onClick={handleUpdate}>
+            <Link to="/updatePortfolio">Update</Link>
+          </button>
+          <button className="btnBox" onClick={handleDelete}>
+            <Link to="/deletePortfolio">Delete</Link>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BoxPortf;
